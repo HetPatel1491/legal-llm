64c167f1050c9289511545c15b894240b4d59444
HetPatel1491 <hetpatel56207@gmail.com>
2026-02-19 11:31:43 -0400

    Add useEffect import to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LandingPage from './LandingPage';
 import ChatPage from './ChatPage';
 import SigninPage from './SigninPage';
